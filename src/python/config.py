a25e724c7855dded0a1596dbaa22f6e9e3dd6169
Darren Roos <darrencraigr@gmail.com>
2024-10-16 10:12:09 +0200

    Add config

---
+++ src/python/config.py
@@ -0,0 +1,34 @@
+import dataclasses
+import json
+import os
+
+@dataclasses.dataclass
+class Config:
+    database: "DatabaseConfig"
+
+
+@dataclasses.dataclass
+class DatabaseConfig:
+    host: str
+    port: int
+    user: str
+    password: str
+    db_name: str
+
+
+def load_config(path: str) -> Config:
+    with open(path, "r") as f:
+        raw_config = json.load(f)
+
+    database_config = DatabaseConfig(
+        **raw_config["database"]
+    )
+
+    config = Config(database=database_config)
+    
+    return config
+
+
+def load_config_from_env() -> Config:
+    CONFIG_PATH = os.getenv("TM_FRAMEWORK_CONFIG_PATH")
+    return load_config(CONFIG_PATH)
---
+++ src/python/tests/config_test.py
@@ -0,0 +1,20 @@
+import pytest
+
+from config import load_config_from_env
+
+
+def test_load_config_from_env():
+    
+    config = load_config_from_env()
+
+    assert config is not None
+
+    assert config.database is not None
+
+    assert config.database is not None
+
+    assert config.database.host != ""
+    assert config.database.port != 0
+    assert config.database.user != ""
+    assert config.database.password != ""
+    assert config.database.db_name != ""
