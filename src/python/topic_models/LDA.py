0c2cb25904ee197c65f285c521fc145fcb56daa9
Darren Roos <darrencraigr@gmail.com>
2025-01-17 07:33:48 +0200

    Correct code

--- src/python/topic_models/LDA.py
+++ src/python/topic_models/LDA.py
@@ -1,5 +1,9 @@
-from corpus import Corpus
 import gensim
+import numpy as np
+
+import configuration as cfg
+from corpus import Corpus
+import database
 
 class LDA:
     def __init__(self, corpus: Corpus, num_topics: int):
@@ -7,22 +11,51 @@ class LDA:
         self.num_topics = num_topics
         self.model = None
         self.corpus_lda = None
+        self.idx2word = None
 
     def train(self):
-        dictionary = gensim.corpora.Dictionary([self.corpus.get_vocabulary()])
         tfidf_vector_obj = self.corpus.get_document_vectors('tfidf')
-        tfidf_vectors = [tfidf_vector.vector for tfidf_vector in tfidf_vector_obj]
-        self.corpus_lda = gensim.matutils.Sparse2Corpus(tfidf_vectors)
+        tfidf_vectors = np.array([tfidf_vector.vector[0] for tfidf_vector in tfidf_vector_obj])
+        self.corpus_lda = gensim.matutils.Scipy2Corpus(tfidf_vectors)
+        _idx2word = {vocab_word.id: vocab_word.word for vocab_word in self.corpus.get_vocabulary()}
+        self.idx2word = {idx: word for idx, (_, word) in enumerate(sorted(_idx2word.items()))}
+        
         self.model = gensim.models.LdaModel(
             self.corpus_lda,
             num_topics=self.num_topics,
-            id2word=dictionary,
-            passes=15,
-            random_state=42
+            id2word=self.idx2word,
+            passes=10,
+            random_state=42,
         )
 
-    def get_topics(self) -> list[str]:
-        return self.model.print_topics()
+    def get_topics(self, n_words=10) -> list[str]:
+        topics = self.model.show_topics(
+            num_topics=self.num_topics,
+            num_words=n_words,
+            formatted=False,
+        )
+
+        topic_words = [[word for word, _ in topic] for _, topic in topics]
+
+        return topic_words
+
+
+def run_lda_on_corpus(session, corpus_name: str, num_topics: int):
+    corpus = Corpus(session, corpus_name)
+    lda = LDA(corpus, num_topics)
+    lda.train()
+    return lda.get_topics()
     
 
+if __name__ == '__main__':
+    config = cfg.load_config_from_env()
+    db_config = config.database
 
+    with database.get_session(db_config) as session:
+        topics = run_lda_on_corpus(
+            session,
+            corpus_name='twitter-financial-news-topic-partial',
+            num_topics=10,
+        )
+        for topic in topics:
+            print(topic)
