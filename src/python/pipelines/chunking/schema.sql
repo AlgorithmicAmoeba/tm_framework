6be93313e01a5be0406778a496178ac959b77be9
Darren Roos <darrencraigr@gmail.com>
2025-04-01 06:42:12 +0200

    Remove migration code

--- src/python/pipelines/chunking/schema.sql
+++ src/python/pipelines/chunking/schema.sql
@@ -19,20 +19,3 @@ CREATE TRIGGER set_current_timestamp_updated_at
 BEFORE UPDATE ON pipeline.chunked_document
 FOR EACH ROW
 EXECUTE FUNCTION set_current_timestamp_updated_at();
-
--- Migration code to update the unique constraint
--- Execute this to migrate from the old schema to the new one
-/*
-BEGIN;
-
--- Drop the old constraint
-ALTER TABLE pipeline.chunked_document 
-DROP CONSTRAINT IF EXISTS chunked_document_corpus_name_raw_document_hash_chunk_start_token_count_key;
-
--- Add the new constraint
-ALTER TABLE pipeline.chunked_document 
-ADD CONSTRAINT chunked_document_corpus_name_raw_document_hash_chunk_hash_key 
-UNIQUE (corpus_name, raw_document_hash, chunk_hash);
-
-COMMIT;
-*/
